76a0bcd898dc014b9a7611275549b4b6637c5c45
thibault alexandre <thibault.a@hotmail.com>
2020-11-05 13:54:00 +0100

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "rest-client"
+Ingredient.destroy_all
+response = RestClient.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+repos = JSON.parse(response)
+puts repos
+repos["drinks"].each do |drink|
+  puts drink
+  ingredient = Ingredient.new(
+    name: drink["strIngredient1"]
+  )
+  ingredient.save!
+end
